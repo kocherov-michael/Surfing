f763eceba9d1a09f9ed5c9d633f8a58ca4a8b675
Michael <ko4erov@mail.ru>
2019-04-09 18:57:31 +0300

    first carousel ready

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+	// $(".owl-carousel").owlCarousel({
+		// 	items: 1,
+		// 	loop: true,
+		// 	dots: false,
+		// 	smartSpeed: 2000
+	// });
+
+	const headerSlider = $(".owl-carousel");
+
+	headerSlider.on('initialized.owl.carousel', function(event) {
+		$('.slide-controls-number__active').text(event.item.index+1)
+		$('.slide-controls-number__total').text(event.item.count)
+	});
+
+	headerSlider.owlCarousel({
+		items: 1,
+		// loop: true,
+		dots: false,
+		smartSpeed: 2000
+	});
+
+	$('#headerSliderRight').click(function() {
+		headerSlider.trigger('next.owl.carousel');
+	})
+	$('#headerSliderLeft').click(function() {
+		headerSlider.trigger('prev.owl.carousel');
+	})
+	headerSlider.on('changed.owl.carousel', function(event) {
+		$('.slide-controls-number__active').text(event.item.index+1)
+		$('.slide-controls-number__total').text(event.item.count)
+	});
+
+
+	
 });
