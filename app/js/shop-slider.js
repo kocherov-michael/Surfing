f0d63522c941726367926d4be16ec20755d7aabb
Michael <ko4erov@mail.ru>
2020-01-08 18:00:51 +0300

    shop slider adaptive

--- app/js/shop-slider.js
+++ app/js/shop-slider.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
 
 	const headerSlider = $("#shopSlider");
 	headerSlider.owlCarousel({
-		items: 3,
 		loop: true,
 		dots: false,
 		margin: 2,
-		smartSpeed: 500
+		smartSpeed: 500,
+		responsive: {
+			// breackpoint from 0 up
+			0 : {
+				items: 1
+			},
+			// breackpoint from 1254 up
+			1254 : {
+				items: 3
+			}
+		}
 	});
 
 	// headerSlider.on('initialized.owl.carousel', function(event) {
